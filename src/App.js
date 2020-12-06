e89c44e85fd62d4ccfd31cf1bd7907f46a66c153
Christian Ares <christianaresvillan@gmail.com>
2020-12-06 21:37:29 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home'
+import About from './components/About';
+import NavBar from './components/NavBar';
+import ProjectDetails from './components/ProjectDetails';
+import {projects as Projects} from './components/Projects';
+import { Route, Switch } from 'react-router-dom';
 
-function App() {
+
+class App extends React.Component{
+  render(){
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route path='/about' component={About} />
+        <Route exact path='/projects' component={Projects} />
+        <Route exact path="/projects/:id" component={ProjectDetails} />
+      </Switch>
     </div>
-  );
+  )
+ } 
 }
 
 export default App;
